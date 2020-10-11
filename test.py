1141cc1588d5dcb0072ea8f595107f243fc0652e
Julien-Devos <julien.devos@student.uclouvain.be>
2020-10-11 22:57:17 +0200

    modified test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-# MDR
+#triangle ingnious
+def triangle(n):
+    liste = []
+    for i in range(1, n+2):
+        l = []
+        for j in range(i):
+            l.append(j)
+        liste.append(l)
+    return liste
