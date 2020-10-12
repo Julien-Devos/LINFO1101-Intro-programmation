e2966ad7e365d7a44670218ac5c7b2660e953ab3
Julien-Devos <julien.devos@student.uclouvain.be>
2020-10-12 15:09:46 +0200

    added a nice test

--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-#test
-print("test")
+print("Bonjour, ceci est un test!")
+for i in range(10):
+    name = input("Comment t'appelles tu ?")
+    print(f"Bonjour {name}, ceci est un test!")
+
+print(f"Bon ça va calme toi {name} je connais ton prénom maintenant !")
