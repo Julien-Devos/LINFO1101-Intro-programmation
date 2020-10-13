449b0acecd6b8f4f28ebe74b7dce318aec415069
Julien-Devos <julien.devos@student.uclouvain.be>
2020-10-13 18:06:16 +0200

    changed bioinfoJU.py

--- Mission 4/bioinfoJU.py
+++ Mission 4/bioinfoJU.py
@@ -1,1 +1,14 @@
+def is_adn(s):
+    adn = False
+    if s == "":
+        return False
 
+    for i in s:
+        if i == "a" or i == "A" or i == "t" or i == "T" or i == "c" or i == "C" or i == "g" or i == "G":
+            adn = True
+        else:
+            return False
+
+    return adn
+
+print(is_adn("aAaza"))
