22c8c3ff8cb10d1bec5850921ebb154894d9ead6
Julien-Devos <julien.devos@student.uclouvain.be>
2020-10-13 20:18:34 +0200

    modified bioinfoJU.py

--- Mission 4/bioinfoJU.py
+++ Mission 4/bioinfoJU.py
@@ -1,24 +1,22 @@
-def positions(s, p):
-    pos = []
-    l = []
-    occur = 0
+def distance_h(s, p):
+    if len(s) != len(p):
+        return None
+    count = 0
     current = 0
-    for i in s.lower():
-        if i == p[occur].lower():
-            occur += 1
-            l.append(current)
-        elif i == p[0].lower():
-            l = []
-            l.append(current)
-            occur = 1
-        else:
-            l = []
-            occur = 0
-        if occur == len(p):
-            pos.append(l[0])
-            l = []
-            occur = 0
+    for e in s:
+        if e != p[current]:
+            count += 1
         current += 1
-    return pos
+    return count
 
+def distances_matrice(l):
+    matrix = []
+    for i in l:
+        line = []
+        for j in l:
+            line.append(distance_h(i, j))
+        matrix.append(line)
+    return matrix
+
+print(distances_matrice(["AG", "AT", "GT", "ACG", "ACT" ]))
 
