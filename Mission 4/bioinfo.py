3deab981fe331e110f0a24ffcb5c3d655f579e7f
Julien-Devos <julien.devos@student.uclouvain.be>
2020-10-13 19:17:36 +0200

    modified bioinfo.py

--- Mission 4/bioinfo.py
+++ Mission 4/bioinfo.py
@@ -9,3 +9,26 @@ def is_adn(s):
             return False
 
     return True
+
+def positions(s, p):
+    pos = []
+    l = []
+    occur = 0
+    current = 0
+    for i in s.lower():
+        if i == p[occur].lower():
+            occur += 1
+            l.append(current)
+        elif i == p[0].lower():
+            l = []
+            l.append(current)
+            occur = 1
+        else:
+            l = []
+            occur = 0
+        if occur == len(p):
+            pos.append(l[0])
+            l = []
+            occur = 0
+        current += 1
+    return pos
