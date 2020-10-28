43ecbcf983d4666f43ab1d1fe0a97ba1dc11e2b9
Julien-Devos <julien.devos@student.uclouvain.be>
2020-10-28 16:26:26 +0100

    modified assistant.py

--- Mission 6/test.py
+++ Mission 6/test.py
@@ -13,19 +13,31 @@ def test_search():
     assert assist.search("this") == "'this' is in the Dictionary", 'test 1'
     assert assist.search("bitch") == "'bitch' is in the Dictionary", 'test 2'
     assert assist.search("cunt") == "'cunt' is in the Dictionary", 'test 3'
-    assert assist.search
+    assert assist.search("bryan") == "'bryan' is not in the Dictionary", 'test 4'
+    assert assist.search("aubergine") == "'aubergine' is not in the Dictionary", 'test 5'
 
 def test_sum():
-    assert assist.sum([4, 6, 5, 3, 9, 2]) == "La somme vaut : 29", 'test 1'
+    assert assist.sum([4, 6, 5, 3, 9, 2]) == 29, 'test 1'
+    assert assist.sum([2, 5, 7, 1]) == 15, 'test 2'
+    assert assist.sum([2, 3, 9, 6, 1]) == 21, 'test 3'
 
 def test_avg():
-    assert assist.avg([4, 6, 5, 3, 9, 2]) == ""
+    assert assist.avg([4, 6, 5, 3, 9, 2]) == "La moyenne est de: 4.833333333333333", 'test 1'
+    assert assist.avg([3, 2, 7, 4]) == "La moyenne est de: 4.0", 'test 2'
+    assert assist.avg([10, 24, 78, 17]) == "La moyenne est de: 32.25", 'test 3'
 
 def test_help():
-
+    assert assist.help() == ("- file <name>: spécifie le nom d'un fichier sur lequel l'outil doit travailler à partir de ce moment\n"
+                            "- info: montre le nombre de lignes et de caractères du fichier \n"
+                            "- dictionary: utilise le fichier comme dictionnaire à partir de maintenant\n"
+                            "- search <word>: détermine si le mot est dans le dictionnaire\n"
+                            "- sum <number1> ... <numbern>: calcule la somme des nombres spécifiés\n"
+                            "- avg <number1> ... <numbern>: calcule la moyenne des nombres spécifiés\n"
+                            "- help: montre des instructions à l'utilisateur\n"
+                            "- exit: arrête l'outil\n")
 
 def test_exit():
-
+    assert assist.exit() == "Stopping program", 'test 1'
 
 def all_tests():
     test_file()
