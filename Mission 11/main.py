ab9c2d112b584fea3847714e22a9249012b2c2cb
Julien-Devos <julien.devos@student.uclouvain.be>
2020-12-01 12:22:35 +0100

    updated mission 11

--- Mission 11/main.py
+++ Mission 11/main.py
@@ -1,62 +1,60 @@
-class Coureur:
+import random
+import time
+from coureur    import Coureur
+from classement import Classement
+from temps      import Temps
+from resultat   import Resultat
+
+class Main :
     """
-    ReprÃ©sente un coureur cycliste.
+    Classe principale pour la mission 11.
     @author  Kim Mens, UCLouvain
     @version 01 DÃ©cembre 2019
     """
 
-    def __init__(self, nom, age):
-        """
-        @pre: nom est un string non-vide
-              age est un entier > 0
-        @post: un coureur nommÃ© nom et Ã¢gÃ© age a Ã©tÃ© crÃ©Ã©
-        """
-        self.__nom = nom  # Le nom du coureur
-        self.__age = age  # L'age du coureur.
+    coureurs = [ Coureur("Alfred", 24), \
+                 Coureur("Bernard", 27), \
+                 Coureur("Claude", 19), \
+                 Coureur("Daniel", 31),  \
+                 Coureur("Emile", 25),  \
+                 Coureur("Fred", 28),  \
+                 Coureur("Gerard", 25) ]
 
-    def nom(self):
-        """
-        MÃ©thode accesseur
-        @pre:  -
-        @post: le nom du coureur a Ã©tÃ© retournÃ©
-        """
-        return self.__nom
+    @classmethod
+    def main(cls) :
+        # CrÃ©er un classement initialement vide pour la course
+        cl = Classement()
+        # Boucle infinie
+        while True :      
+            # Choisir alÃ©atoirement un coureur de la liste
+            c = random.choice(cls.coureurs)
+            # Lui assigner un temps entre 1000 et 5000 secondes
+            t = Temps()
+            t.add_secondes(random.randint(1000, 5000))
+            # CrÃ©er un rÃ©sultat pour ce coureur avec ce temps
+            r = Resultat(c, t)
+            print(r)
+            # Cherche le dernier rÃ©sultat de ce coureur dans le classement.
+            r1 = cl.get(r.coureur())
+            # Imprime le classement actuel de coureur dans le classement.
+            if r1 is None :
+                print("  Pas encore classÃ©")
+            else:
+                print("  Actuellement classÃ© " + str(cl.get_position(c)))
+                print("  Dernier temps enregistrÃ© = " + str(r1.temps()))
+            # Compare son dernier rÃ©sultat stockÃ© avec son nouveau rÃ©sultat
+            if r1 is not None and r >= r1 :
+                print("  Moins bon temps, ignorÃ©")
+            else :
+                print("  Nouveau temps est meilleur; sera enregistrÃ©")
+                cl.remove(c)
+                cl.add(r)
+                print("  Maintenant classÃ© " + str(cl.get_position(c)));
+                print()
+                print("CLASSEMENT:")
+                print(cl)
+                print()
+            # Attendre une seconde avant de recommencer la boucle while   
+            time.sleep(1)
 
-    def age(self):
-        """
-        MÃ©thode accesseur
-        @pre:  -
-        @post: l'Ã¢ge du coureur a Ã©tÃ© retournÃ©
-        """
-        return self.__age
-
-    def __eq__(self, other):
-        """
-        MÃ©thode magique
-        Teste si ce coureur est Ã©gal a un objet quelconque.
-        Le critÃ¨re d'Ã©galitÃ© porte sur le nom et l'Ã¢ge du coureur.
-        @pre:  -
-        @post: Retourne True si other est egal Ã  self (meme type et valeurs des attributs);
-               retourne False sinon.
-        """
-        return (type(other) == Coureur) and (self.nom() == other.nom()) and (self.age() == other.age())
-
-    def __hash__(self):
-        """
-        MÃ©thode magique
-        Retourne un hash code pour cet objet. Ceci est nÃ©cessaire pour utiliser un objet de type
-        Coureur comme clÃ© d'une dictionnaire, comme dans notre implÃ©mentation naÃ¯ve du classement.
-        Remarque: Pour votre implÃ©mentation vous pouvez ignorer cette mÃ©thode; elle n'est pas importante
-                  si on stocke les rÃ©sultats dans une liste chaÃ®nÃ©e plutÃ´t que dans une dictionnaire.
-        """
-        return hash(str(self))
-
-    def __str__(self):
-        """
-        MÃ©thode magique
-        Retourne une reprÃ©sentation string de cet objet.
-        @pre:  -
-        @post: Retourne un reprÃ©sentation avec le nom et l'Ã¢ge de ce coureur,
-               dans le format "Coureur NOM (age AGE)"
-        """
-        return "Coureur " + self.nom() + " (Ã¢ge " + str(self.age()) + ")"
+Main.main()
