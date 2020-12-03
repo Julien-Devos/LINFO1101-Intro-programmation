ec24830ae0b5bbacae7ef0a5944ff81218d7cabc
Julien-Devos <julien.devos@student.uclouvain.be>
2020-12-03 14:12:37 +0100

    End of missions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # UCL-PythonMissions
-Sur ce GitHub se trouve toutes les missions 
+Sur ce GitHub se trouve toutes les phases de réalisations des missions 
 réalisées pour le cours Indroduction à la programmation
 LINFO1101  
--- test.py
+++
@@ -1,6 +0,0 @@
-print("Bonjour, ceci est un test!")
-for i in range(5):
-    name = input("Comment t'appelles tu ?")
-    print(f"Bonjour {name}, ceci est un test!")
-
-print(f"Bon ça va calme toi {name} je connais ton prénom maintenant !") #test
